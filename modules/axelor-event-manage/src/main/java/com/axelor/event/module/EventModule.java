e8b826200b83605c984b4000b374732428232bc1
bma2207 <vickymali076@gmail.com>
2019-09-27 11:18:25 +0530

    code formatting....

--- modules/axelor-event-manage/src/main/java/com/axelor/event/module/EventModule.java
+++ modules/axelor-event-manage/src/main/java/com/axelor/event/module/EventModule.java
@@ -4,13 +4,10 @@ import com.axelor.app.AxelorModule;
 import com.axelor.event.service.EventRegistrationService;
 import com.axelor.event.service.EventRegistrationServiceImp;
 
-public class EventModule extends AxelorModule{
+public class EventModule extends AxelorModule {
 
 	@Override
 	protected void configure() {
-		// TODO Auto-generated method stub
 		bind(EventRegistrationService.class).to(EventRegistrationServiceImp.class);
 	}
-
-	
 }
